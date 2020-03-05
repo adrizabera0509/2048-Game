54124de3b19913874ee9e58abf1c7906150d7601
adrizabera0509 <42275973+adrizabera0509@users.noreply.github.com>
2020-03-05 23:54:41 +0530

    Add file consisting required constatants

---
+++ Constant.py
@@ -0,0 +1,15 @@
+SIZE = 400
+GRID_LEN = 4
+GRID_PADDING = 10
+BACKGROUND_COLOR_GAME = "#616A6B"
+BACKGROUND_COLOR_CELLEMPTY = "#979A9A"
+BACKGROUND_COLOR_DICT = {2: "#eee4da", 4: "#ede0c8", 8: "#f2b179", 16: "#f59563", 32: "#f67c5f", 64: "#f65e3b", 128: "#edcf72", 
+                         256: "#edc610", 512: "#edc850", 1024: "#edc53f", 2048: "edc22e" }
+CELL_COLOR_DICT = {2: "#776e65", 4:"#776e65", 8: "#f9f6f2", 16:"#f9f6f2", 32:"#f9f6f2", 64:"#f9f6f2", 128: "#f9f6f2", 
+                   256:"#f9f6f2", 512: "#f9f6f2", 1024: "#f9f6f2", 2048: "#f9f6f2"}
+FONT = ("Verdana", 40,"bold")
+
+KEY_UP = "'w'"
+KEY_DOWN = "'s'"
+KEY_LEFT = "'a'"
+KEY_RIGHT = "'d'"
